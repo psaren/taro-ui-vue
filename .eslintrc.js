74181416560fdb0c47563067ddde354bc38a4d6d
pengshanglong <pengshanglong@simuwang.com>
2020-05-12 18:14:12 +0800

    chore: prettier format code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,27 @@
 // ESLint 检查 .vue 文件需要单独配置编辑器：
 // https://eslint.vuejs.org/user-guide/#editor-integrations
 module.exports = {
-  'extends': ['taro/vue']
+  'extends': [
+    "plugin:prettier/recommended",
+    "prettier/vue",
+    'taro/vue'
+  ],
+  parserOptions: { ecmaVersion: 2015, ecmaFeatures: { legacyDecorators: true } },
+  rules: {
+    "no-unused-vars": ["error", { "vars": "all", "args": "none" }],
+    "prettier/prettier": [
+      "error",
+      {
+        "semi": false,
+        "singleQuote": true,
+        "bracketSpacing": true,
+        "jsxBracketSameLine": true,
+        "trailingComma": "es5",
+        "jsxSingleQuote": false,
+        "parser": "flow",
+        "endOfLine":"auto",
+        "printWidth": 100
+      }
+    ],
+  }
 }
