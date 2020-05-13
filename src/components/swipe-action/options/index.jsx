5b0c2f12cc75a0b84ea5a2097363b6c5b330025b
pengshanglong <pengshanglong@simuwang.com>
2020-05-13 13:59:34 +0800

    feat: add SwipeAction

---
+++ src/components/swipe-action/options/index.jsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames'
+import { delayQuerySelector } from '../../../utils/common'
+
+export default {
+  name: 'SwipeActionOptions',
+  props: {
+    componentId: {
+      type: String,
+      default: '',
+    },
+    onQueryedDom: {
+      type: Function,
+      default: () => () => {},
+    },
+    className: {
+      type: [Array, String],
+      default: () => '',
+    },
+  },
+  methods: {
+    trrigerOptionsDomUpadte() {
+      delayQuerySelector(this, `#swipeActionOptions-${this.componentId}`).then((res) => {
+        this.onQueryedDom(res[0])
+      })
+    },
+  },
+  render() {
+    const rootClass = classNames('at-swipe-action__options', this.className)
+
+    return (
+      <view id={`swipeActionOptions-${this.componentId}`} class={rootClass}>
+        {this.$slots.default}
+      </view>
+    )
+  },
+}
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,5 +1,7 @@
 import Taro from '@tarojs/taro'
+import { SelectorQuery } from '@tarojs/taro/types/index'
 const { getEnv, ENV_TYPE } = Taro
+const ENV = Taro.getEnv()
 
 type ENVS = {
   isWEAPP: boolean,
@@ -16,4 +18,96 @@ const getEnvs = (): ENVS => {
   }
 }
 
-export { getEnvs }
+function delay(delayTime = 500): Promise<null> {
+  return new Promise((resolve) => {
+    if ([Taro.ENV_TYPE.WEB, Taro.ENV_TYPE.SWAN].includes(ENV)) {
+      setTimeout(() => {
+        resolve()
+      }, delayTime)
+      return
+    }
+    resolve()
+  })
+}
+
+function delayGetScrollOffset({ delayTime = 500 }): Promise<[]> {
+  return new Promise((resolve) => {
+    delay(delayTime).then(() => {
+      Taro.createSelectorQuery()
+        .selectViewport()
+        .scrollOffset()
+        .exec((res: []) => {
+          resolve(res)
+        })
+    })
+  })
+}
+
+function delayGetClientRect({ _, selectorStr, delayTime = 500 }): Promise<[]> {
+  false && console.log(_)
+  const selector: SelectorQuery = Taro.createSelectorQuery()
+
+  return new Promise((resolve) => {
+    delay(delayTime).then(() => {
+      selector
+        .select(selectorStr)
+        .boundingClientRect()
+        .exec((res: []) => {
+          resolve(res)
+        })
+    })
+  })
+}
+
+function delayQuerySelector(self, selectorStr: string, delayTime = 500): Promise<[]> {
+  const $scope = ENV === Taro.ENV_TYPE.WEB ? self : self.$scope
+  const selector: SelectorQuery = Taro.createSelectorQuery().in($scope)
+
+  return new Promise((resolve) => {
+    delay(delayTime).then(() => {
+      selector
+        .select(selectorStr)
+        .boundingClientRect()
+        .exec((res: []) => {
+          resolve(res)
+        })
+    })
+  })
+}
+
+function uuid(len = 8, radix = 16): string {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('')
+  const value: string[] = []
+  let i = 0
+  radix = radix || chars.length
+
+  if (len) {
+    // Compact form
+    for (i = 0; i < len; i++) value[i] = chars[0 | (Math.random() * radix)]
+  } else {
+    // rfc4122, version 4 form
+    let r
+
+    // rfc4122 requires these characters
+    /* eslint-disable-next-line */
+    value[8] = value[13] = value[18] = value[23] = '-'
+    value[14] = '4'
+
+    // Fill in random data.  At i==19 set the high bits of clock sequence as
+    // per rfc4122, sec. 4.1.5
+    for (i = 0; i < 36; i++) {
+      if (!value[i]) {
+        r = 0 | (Math.random() * 16)
+        value[i] = chars[i === 19 ? (r & 0x3) | 0x8 : r]
+      }
+    }
+  }
+
+  return value.join('')
+}
+
+function isTest(): boolean {
+  return process.env.NODE_ENV === 'test'
+}
+
+export { getEnvs, delayGetScrollOffset, delayGetClientRect, delayQuerySelector, uuid, isTest }
