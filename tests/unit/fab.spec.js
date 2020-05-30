352be120e3a8fac349b82b27f363103fdbaf95c5
psaren <18718574603@163.com>
2020-05-30 13:39:09 +0800

    test(fab): 新增 fab 测试用例

---
+++ tests/unit/fab.spec.js
@@ -0,0 +1,36 @@
+import { mount } from '@vue/test-utils'
+import AtFab from '../../src/components/fab'
+
+const factory = (values = {}, slots = { default: ['按钮'] }) => {
+  return mount(AtFab, {
+    components: {},
+    slots,
+    propsData: { ...values },
+  })
+}
+
+describe('AtFab Snap', () => {
+  it('render AtFab -- default props', () => {
+    const wrapper = factory()
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('render AtFab -- props className', () => {
+    const wrapper = factory({ className: 'button' })
+    expect(wrapper.element).toMatchSnapshot()
+  })
+
+  it('render AtFab -- props size small', () => {
+    const wrapper = factory({ size: 'small' })
+    expect(wrapper.element).toMatchSnapshot()
+  })
+})
+
+describe('AtFab Event', () => {
+  it('AtFab onClick', () => {
+    const onClick = jest.fn()
+    const wrapper = factory({ onClick: onClick })
+    wrapper.find('.at-fab').trigger('tap')
+    expect(wrapper.element).toMatchSnapshot()
+  })
+})
