13f67f0fcf076a68111200458eebad6404f952e3
pengsl <18718574603@163.com>
2020-07-10 23:38:03 +0800

    feat(taro-ui-vue): rewrite checkbox

---
+++ packages/taro-ui-vue/src/components/checkbox/index.ts
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+import classNames from 'classnames'
+
+const AtCheckbox = Vue.extend({
+  name: 'AtCheckbox',
+  props: {
+    customStyle: {
+      type: [Object, String],
+      default: function () {
+        return {}
+      },
+    },
+    className: {
+      type: [Object, String],
+      default: function () {
+        return {}
+      },
+    },
+    options: {
+      type: Array,
+      default: function () {
+        return []
+      },
+    },
+    selectedList: {
+      type: Array,
+      default: function () {
+        return []
+      },
+    },
+    onChange: {
+      type: Function,
+      default: function () {
+        return function () {}
+      },
+    },
+  },
+  methods: {
+    handleClick(idx: number): void {
+      const { selectedList, options } = this
+      const option = options[idx]
+      const { disabled, value } = option
+      if (disabled) return
+
+      const selectedSet = new Set(selectedList)
+      if (!selectedSet.has(value)) {
+        selectedSet.add(value)
+      } else {
+        selectedSet.delete(value)
+      }
+      this.onChange([...selectedSet])
+    },
+    getOptionCls(option) {
+      const { disabled, value } = option
+      return classNames('at-checkbox__option', {
+        'at-checkbox__option--disabled': disabled,
+        'at-checkbox__option--selected': this.selectedList.includes(value),
+      })
+    }
+  },
+})
+
+export default AtCheckbox
