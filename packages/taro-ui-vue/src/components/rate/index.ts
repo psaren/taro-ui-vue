e2d2266c518e19486d400fd5e91d0b7cc7db5832
pengsl <18718574603@163.com>
2020-07-11 10:34:06 +0800

    feat(taro-ui-vue): rewrite rate

---
+++ packages/taro-ui-vue/src/components/rate/index.ts
@@ -0,0 +1,83 @@
+import Vue from 'vue'
+import { CommonEvent } from '@tarojs/components/types/common'
+import classNames from 'classnames'
+import { pxTransform } from '../../utils/common'
+
+const AtRate = Vue.extend({
+  name: 'AtRate',
+  props: {
+    customStyle: {
+      type: [Object, String],
+      default: function () {
+        return {}
+      },
+    },
+    className: {
+      type: [Object, String],
+      default: function () {
+        return {}
+      },
+    },
+    size: {
+      type: [Number, String],
+      default: 0,
+    },
+    value: {
+      type: Number,
+      default: 0,
+    },
+    max: {
+      type: Number,
+      default: 5,
+    },
+    margin: {
+      type: Number,
+      default: 5,
+    },
+    onChange: {
+      type: Function,
+      default: function () {
+        return function () {}
+      },
+    },
+  },
+  computed: {
+    rootCls () {
+      return classNames('at-rate', this.className)
+    },
+    iconStyle () {
+      return {
+        marginRight: pxTransform(this.margin)
+      }
+    },
+    starIconStyle () {
+      const { size } = this
+      return {
+        fontSize: size ? `${size}px` : ''
+      }
+    },
+    classNameArr (): string[] {
+      const { value, max }= this
+      const arr: string[]= []
+      const floorValue = Math.floor(value)
+      const ceilValue = Math.ceil(value)
+      for (let i = 0; i < max; i++) {
+        if (floorValue > i) {
+          arr.push('at-rate__icon at-rate__icon--on')
+        } else if (ceilValue - 1 === i) {
+          arr.push('at-rate__icon at-rate__icon--half')
+        } else {
+          arr.push('at-rate__icon at-rate__icon--off')
+        }
+      }
+      return arr
+    }
+  },
+  methods: {
+    handleClick(event: CommonEvent) {
+      this.onChange && this.onChange(event)
+    },
+  },
+})
+
+export default AtRate
