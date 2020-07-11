a51539a0950d4ee08757b1397548af017e6a6dd3
pengsl <18718574603@163.com>
2020-07-11 11:31:07 +0800

    feat(taro-ui-vue): rewrite tag

---
+++ packages/taro-ui-vue/src/components/tag/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view
+    :class="rootCls"
+    :style="customStyle"
+    @tap="handleClick"
+  >
+    <slot></slot>
+  </view>
+</template>
+
+<script lang="ts">
+import AtTag from './index'
+export default AtTag
+</script>
