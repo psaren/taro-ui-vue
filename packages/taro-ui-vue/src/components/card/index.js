b6f406d80fa91cb0701f740fa58b8c5d64d28c18
pengsl <18718574603@163.com>
2020-07-18 00:21:29 +0800

    refactor(taro-ui-vue): rewrite card

---
+++ packages/taro-ui-vue/src/components/card/index.js
@@ -0,0 +1,80 @@
+import classNames from 'classnames'
+
+export default {
+  name: 'AtCard',
+  props: {
+    note: {
+      type: String,
+      default: '',
+    },
+    isFull: {
+      type: Boolean,
+      default: false,
+    },
+    thumb: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    extra: {
+      type: String,
+      default: '',
+    },
+    icon: {
+      type: Object,
+      default: () => {},
+    },
+    onClick: {
+      type: Function,
+      default: () => () => {},
+    },
+    renderIcon: {
+      type: String,
+      default: '',
+    },
+    extraStyle: {
+      type: Object,
+      default: () => {},
+    },
+    className: {
+      type: [String, Array],
+      default: '',
+    },
+  },
+  computed: {
+    rootClass() {
+      return classNames(
+        'at-card',
+        {
+          'at-card--full': this.isFull,
+        },
+        this.className
+      )
+    },
+    iconClass() {
+      const { icon } = this
+      return classNames({
+        'at-icon': true,
+        [`at-icon-${icon && icon.value}`]: icon && icon.value,
+        'at-card__header-icon': true,
+      })
+    }, 
+    iconStyle() {
+      const { icon } = this
+      return {
+        color: (icon && icon.color) || '',
+        fontSize: (icon && `${icon.size}px`) || '',
+      }
+    }
+  },
+  methods: {
+    handleClick(args) {
+      if (typeof this.onClick === 'function') {
+        this.onClick(args)
+      }
+    },
+  },
+}
---
+++ packages/taro-ui-vue/src/components/card/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <view 
+    @tap="handleClick"  
+    class="rootClass">
+    <view class="at-card__header">
+      <view v-if="thumb" class="at-card__header-thumb">
+        <image class="at-card__header-thumb-info" mode="scaleToFill" :src="thumb" />
+      </view>
+      <slot name="renderIcon">{{ renderIcon }}</slot>
+      <view v-if="!thumb && icon && icon.value" :class="iconClass" :style="iconStyle"></view>
+      <view class="at-card__header-title">{{ title }}</view>
+      <view v-if="extra" :style="{ ...extraStyle }" class="at-card__header-extra">
+        {{ extra }}
+      </view>
+    </view>
+    <view class="at-card__content">
+      <view class="at-card__content-info">
+        <slot></slot>
+      </view>
+      <view v-if="note" class="at-card__content-note">{{ note }}</view>
+    </view>
+  </view>
+</template>
+<script>
+import AtCard from './index'
+export default AtCard
+</script>
