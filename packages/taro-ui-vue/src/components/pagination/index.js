44c9afc99a832db2452fb855a0fdab17c46366cf
pengsl <18718574603@163.com>
2020-07-18 20:17:20 +0800

    refactor:(taro-ui-vue): rewrite pagination

---
+++ packages/taro-ui-vue/src/components/pagination/index.js
@@ -0,0 +1,111 @@
+import classNames from 'classnames'
+import mixins from '../mixins'
+import AtButton from '../button/index'
+
+const MIN_MAXPAGE = 1
+/**
+ *
+ * @param {number} maxPage
+ */
+const getMaxPage = (maxPage = 0) => {
+  if (maxPage <= 0) return MIN_MAXPAGE
+  return maxPage
+}
+/**
+ *
+ * @param {number} maxPage
+ */
+const createPickerRange = (max) => {
+  const range = new Array(max).fill(0).map((val, index) => index + 1)
+  return range
+}
+
+export default {
+  name: 'AtPagination',
+  mixins: [mixins],
+  props: {
+    customStyle: {
+      type: [Object, String],
+      default: () => '',
+    },
+    className: {
+      type: [Array, String],
+      default: () => '',
+    },
+    current: {
+      type: Number,
+      default: 1,
+    },
+    total: {
+      type: Number,
+      default: 0,
+    },
+    pageSize: {
+      type: Number,
+      default: 20,
+    },
+    icon: {
+      type: Boolean,
+      default: false,
+    },
+    onPageChange: {
+      type: Function,
+      default: () => () => {},
+    },
+  },
+  computed: {
+    rootCls () {
+      const rootClassName = ['at-pagination']
+      const classObject = {
+        'at-pagination--icon': this.icon,
+      }
+      return classNames(rootClassName, classObject, this.className)
+    },
+    prevDisabled() {
+      const { maxPage, currentPage } = this.state
+      return maxPage === MIN_MAXPAGE || currentPage === 1
+    },
+    nextDisabled() {
+      const { maxPage, currentPage } = this.state
+      return maxPage === MIN_MAXPAGE || currentPage === maxPage
+    }
+  },
+  data() {
+    const { current, pageSize, total } = this
+    const maxPage = getMaxPage(Math.ceil(total / pageSize))
+    return {
+      state: {
+        currentPage: current || 1,
+        maxPage,
+        pickerRange: createPickerRange(maxPage),
+      },
+    }
+  },
+  watch: {
+    total() {
+      const { pageSize, total } = this
+      this.state.maxPage = getMaxPage(Math.ceil(total / pageSize))
+    }
+  },
+  methods: {
+    onPrev() {
+      let { currentPage } = this.state
+      const originCur = currentPage
+      currentPage -= 1
+      currentPage = Math.max(1, currentPage)
+      if (originCur === currentPage) return
+      this.onPageChange && this.onPageChange({ type: 'prev', current: currentPage })
+      this.setState({ currentPage })
+    },
+    onNext() {
+      let { currentPage } = this.state
+      const originCur = currentPage
+      const { maxPage } = this.state
+      currentPage += 1
+      currentPage = Math.min(maxPage, currentPage)
+      if (originCur === currentPage) return
+      this.onPageChange && this.onPageChange({ type: 'next', current: currentPage })
+      this.setState({ currentPage })
+    },
+  },
+}
---
+++ packages/taro-ui-vue/src/components/pagination/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <view 
+    :class="rootCls" 
+    :style="customStyle">
+    <view class="at-pagination__btn-prev">
+      <AtButton v-if="icon" :onClick.props="onPrev" size="small" :disabled="prevDisabled">
+        <view class="at-icon at-icon-chevron-left text"></view>
+      </AtButton>
+      <AtButton v-else :onClick.props="onPrev" size="small" :disabled="prevDisabled">
+        上一页
+      </AtButton>
+    </view>
+    <view class="at-pagination__number">
+      <view class="at-pagination__number-current text">{{ state.currentPage }}</view>/{{ state.maxPage }}
+    </view>
+    <view class="at-pagination__btn-next">
+      <AtButton v-if="icon" :onClick.props="onNext" size="small" :disabled="nextDisabled">
+        <view class="at-icon at-icon-chevron-right text"></view>
+      </AtButton>
+      <AtButton v-else :onClick.props="onNext" size="small" :disabled="nextDisabled">
+        下一页
+      </AtButton>
+    </view>
+  </view>
+</template>
+<script>
+import AtPagination from './index'
+export default AtPagination
+</script>
