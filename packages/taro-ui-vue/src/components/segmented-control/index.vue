a6cbd6ad9023b2ce5d462d137dee0441bd8f9103
pengsl <18718574603@163.com>
2020-07-18 21:33:07 +0800

    refactor(taro-ui-vue): rewrite segmented-control

---
+++ packages/taro-ui-vue/src/components/segmented-control/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <view 
+    :class="rootCls" 
+    :style="rootSty"
+  >
+    <view
+      v-for="(value, i) in values"
+      :class="classNames('at-segmented-control__item', {
+        'at-segmented-control__item--active': current === i,
+      })"
+      :style="current === i ? selectedItemStyle : itemStyle"
+      :key="value"
+      @tap="handleClick(i, $event)"
+    >
+      {{ value }}
+    </view>
+  </view>
+</template>
+
+<script>
+import AtSegmentedControl from './index'
+export default AtSegmentedControl
+</script>
