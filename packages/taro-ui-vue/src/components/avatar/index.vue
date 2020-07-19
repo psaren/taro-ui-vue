860cf9c8b5b6bdd70c86778275e9e8810ee8e024
pengsl <18718574603@163.com>
2020-07-19 16:49:34 +0800

    refactor(taro-ui-vue): rewrite avatar

---
+++ packages/taro-ui-vue/src/components/avatar/index.vue
@@ -0,0 +1,11 @@
+<template>
+   <view :class="rootCls" :style="customStyle">
+      <OpenData v-if="isOpenData" :type="openData.type"></OpenData>
+      <image v-if="isImage" class="at-avatar__img" :src="image" />
+      <view v-if="isText" class="at-avatar__text">{{ letter }}</view>
+    </view>
+</template>
+<script>
+import AtAvatar from './index'
+export default AtAvatar
+</script>
