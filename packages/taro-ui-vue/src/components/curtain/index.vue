0527b234f52bfea8c5a5e3b9efdc518df89da455
pengsl <18718574603@163.com>
2020-07-19 17:26:24 +0800

    refactor(taro-ui-vue): rewrite curtain

---
+++ packages/taro-ui-vue/src/components/curtain/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <view 
+    :class="curtainClass" 
+    :style="customStyle" 
+    @tap="stopPropagation"
+  >
+    <view class="at-curtain__container">
+      <view class="at-curtain__body">
+        <slot></slot>
+        <view :class="btnCloseClass" @tap="handleTab"></view>
+      </view>
+    </view>
+  </view>
+</template>
+<script>
+import AtCurtain from './index'
+export default AtCurtain
+</script>
